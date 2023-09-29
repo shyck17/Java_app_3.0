d3779c14df6c8b971dfa3c399dafbf1e8d993203
Shyam Chandran Koodathinal <145469957+shyck17@users.noreply.github.com>
2023-09-28 22:08:25 -0400

    Add files via upload

---
+++ froggy.py
@@ -0,0 +1,28 @@
+import requests
+
+def transfer():
+    print('heello')
+    mavenpath=' /home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar '
+    jfrogpath=' http://13.52.211.102:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar '
+    username = 'admin'
+    password ='test123' 
+
+    with open (mavenpath,'rb') as mpath:
+        res=requests.put(jfrogpath,auth=(username,password),data=mpath)
+
+    if res.status_code == 200:
+        print('File transferred successfully')
+    else:
+        print(f'file transfer failed with {res.status_code} and {res.text} ')
+
+def main():
+    transfer()
+    print('Hello')
+
+if __name__ == '__main__':
+    main()
+
+
+
+    
+    
